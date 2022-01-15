3f459977e4f485625d46e9aac19d200282019fea
luffy1610 <luffy1610@gmail.com>
2022-01-15 16:00:16 +0800

    format: formatted app.py

--- graphql/app.py
+++ graphql/app.py
@@ -6,13 +6,11 @@ from schema.schema import graphql_schema
 app = Flask(__name__)
 
 
-app.add_url_rule('/graphql',\
-    view_func=GraphQLView.as_view(
-        'graphql',
-        schema=graphql_schema(),
-        graphiql=True
-    )
+app.add_url_rule(
+    "/graphql",
+    view_func=GraphQLView.as_view("graphql", schema=graphql_schema(), graphiql=True),
 )
+
 
 @app.route("/")
 def index():
