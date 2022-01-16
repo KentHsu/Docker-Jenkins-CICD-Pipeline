0098bc1c7b4c73909d8f6cce4af76d1e8d380aaf
luffy1610 <luffy1610@gmail.com>
2022-01-16 14:13:21 +0800

    feat: added AuthorType query

--- graphql/schema/schema.py
+++ graphql/schema/schema.py
@@ -1,12 +1,19 @@
 from graphene import ObjectType, Schema, Field
-from graphene import ID, String, List
+from graphene import ID, String, Int
 
 
 books = [
-    { "name": 'Name of the Wind', "genre": 'Fantasy', "id": '1' },
-    { "name": 'The Final Empire', "genre": 'Fantasy', "id": '2' },
-    { "name": 'The Long Earth', "genre": 'Sci-Fi', "id": '3' },
+    {"name": "Name of the Wind", "genre": "Fantasy", "id": "1"},
+    {"name": "The Final Empire", "genre": "Fantasy", "id": "2"},
+    {"name": "The Long Earth", "genre": "Sci-Fi", "id": "3"},
 ]
+
+authors = [
+    {"name": "Patrick Rothfuss", "age": 44, "id": "1"},
+    {"name": "Brandon Sanderson", "age": 42, "id": "2"},
+    {"name": "Terry Pratchett", "age": 66, "id": "3"},
+]
+
 
 class BookType(ObjectType):
     id = ID()
@@ -14,9 +21,16 @@ class BookType(ObjectType):
     genre = String()
 
 
+class AuthorType(ObjectType):
+    id = ID()
+    name = String()
+    age = Int()
+
+
 class RootQuery(ObjectType):
 
-    book = Field(BookType, id = ID())
+    book = Field(BookType, id=ID())
+    author = Field(AuthorType, id=ID())
 
     def resolve_book(parent, info, id):
         for book in books:
@@ -24,9 +38,13 @@ class RootQuery(ObjectType):
                 return book
         return None
 
+    def resolve_author(parent, info, id):
+        for author in authors:
+            if author["id"] == id:
+                return author
+        return None
+
 
 def graphql_schema():
-    schema = Schema(
-        query=RootQuery
-    )
+    schema = Schema(query=RootQuery)
     return schema
