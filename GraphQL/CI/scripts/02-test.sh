86b5a4f9636ab0e81761998dcac7ce400f8f7b35
luffy1610 <luffy1610@gmail.com>
2022-02-04 14:10:05 +0800

    chore: added compose down after CI test stage

--- GraphQL/CI/scripts/02-test.sh
+++ GraphQL/CI/scripts/02-test.sh
@@ -1,1 +1,1 @@
-docker-compose up -d && docker exec graphql_graphql-app_1 venv/bin/python3 -m pytest
+docker-compose up -d && docker exec graphql_graphql-app_1 venv/bin/python3 -m pytest && docker-compose down
